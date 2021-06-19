bfa4b9bdcb47aea501289e2f279195e650d95b7f
ajaykumar6768 <74002416+ajaykumar6768@users.noreply.github.com>
2021-06-19 19:12:00 +0530

    Create Welcome to Java.java

---
+++ public class Solution {      public static void main(String[] args) {/* Enter your code here. Print output to STDOUT. Your class should be named Solution. */Welcome to Java.java
@@ -0,0 +1,15 @@
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Print output to STDOUT. Your class should be named Solution. */
+    }
+}
+
+#Sample Input :
+
+There is no input for this challenge.
+
+#Sample Output :
+
+Hello, World.
+Hello, Java.
