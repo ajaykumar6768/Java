1e0afa70ee41cfc2d6948382031f29ba3ccde19f
ajaykumar6768 <74002416+ajaykumar6768@users.noreply.github.com>
2021-06-22 22:48:14 +0530

    Create Welcome to Java!.java

---
+++ Welcome to Java!.java
@@ -0,0 +1,16 @@
+public class Solution {
+
+    public static void main(String[] args) 
+    {
+        System.out.println("Hello, World.");
+        System.out.println("Hello, Java.");
+    }
+}
+
+
+
+
+#Sample output :
+
+Hello, World.
+Hello, Java.
