6555546ff82234671ead9539c395031fa73ab632
ajaykumar6768 <74002416+ajaykumar6768@users.noreply.github.com>
2021-07-03 21:44:11 +0530

    Create Java Int to String.java

---
+++ Java Int to String.java
@@ -0,0 +1,26 @@
+import java.util.*;
+import java.security.*;
+public class Solution {
+ public static void main(String[] args) {
+
+  DoNotTerminate.forbidExit();
+
+  try {
+   Scanner in = new Scanner(System.in);
+   int n = in .nextInt();
+   in.close();
+   //String s=???; Complete this line below
+
+
+
+   //Write your code here
+            String s = String.valueOf(n);
+
+
+#Sample Input :
+    
+100
+ 
+#Sample Output :
+ 
+Good job
