738981954e086ad8cf3a8643c4e4f9f2fc44eb2b
ajaykumar6768 <74002416+ajaykumar6768@users.noreply.github.com>
2021-07-08 13:54:09 +0530

    Create Java String Reverse.java

---
+++ Java String Reverse.java
@@ -0,0 +1,33 @@
+import java.util.*;
+public class Palindrome
+{
+    public static void main(String args[])
+    {
+        String a, b = "";
+        Scanner s = new Scanner(System.in);
+        a = s.nextLine();
+        int n = a.length();
+        for(int i = n - 1; i >= 0; i--)
+        {
+            b = b + a.charAt(i);
+        }
+        if(a.equalsIgnoreCase(b))
+        {
+            System.out.println("Yes");
+        }
+        else
+        {
+            System.out.println("No");
+        }
+    }
+}
+
+
+#Sample Input :
+
+madam
+
+
+#Sample Output :
+
+Yes
