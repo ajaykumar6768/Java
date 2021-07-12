32b58697dff9777441a0fa6823ada8562b51c278
ajaykumar6768 <74002416+ajaykumar6768@users.noreply.github.com>
2021-07-12 13:56:05 +0530

    Create Java BigInteger.java

---
+++ Java BigInteger.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) 
+    {
+        Scanner sc =new Scanner(System.in);
+        BigInteger d=sc.nextBigInteger();
+        BigInteger c=sc.nextBigInteger();
+        System.out.println(d.add(c));
+        System.out.println(d.multiply(c));
+    }
+}
+
+
+
+#Sample Input :
+
+1234
+20
+  
+#Sample Output :
+
+1254
+24680
