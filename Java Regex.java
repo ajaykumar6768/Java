42d57a09e8d330b7be34abaadce3a6e6f009013b
ajaykumar6768 <74002416+ajaykumar6768@users.noreply.github.com>
2021-07-18 18:31:04 +0530

    Create Java Regex.java

---
+++ Java Regex.java
@@ -0,0 +1,37 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.Scanner;
+
+class Solution{
+
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        while(in.hasNext()){
+            String IP = in.next();
+            System.out.println(IP.matches(new MyRegex().pattern));
+        }
+
+    }
+}
+class MyRegex {
+    String num = "([01]?\\d{1,2}|2[0-4]\\d|25[0-5])";
+    String pattern = num + "." +  num + "." +  num + "." + num;
+}
+
+#Sample Input :
+
+000.12.12.034
+121.234.12.12
+23.45.12.56
+00.12.123.123123.123
+122.23
+Hello.IP
+
+#Sample Output :
+
+true
+true
+true
+false
+false
+false
